a0ffc0a7dd5abfcdfbccd187aff71888861e5257
ritsiiitd <ritick20460@iiitd.ac.in>
2023-12-08 19:31:40 +0530

    hamburger issue

---
+++ nss-frontend/src/components/Hamburger.jsx
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+import {
+  MDBContainer,
+  MDBCollapse,
+  MDBNavbar,
+  MDBNavbarToggler,
+  MDBIcon,
+  MDBBtn,
+} from 'mdb-react-ui-kit';
+
+import {Link} from 'react-router-dom';
+import { useAuth0 } from "@auth0/auth0-react";
+import { useEffect } from 'react';
+import { logoutIcon } from '../assets/images';
+
+export default function App() {
+  const [showAnimated, setShowAnimated] = useState(false);
+  const [showAnimated2, setShowAnimated2] = useState(false);
+  const [showAnimated3, setShowAnimated3] = useState(false);
+  const {user, logout , loginWithRedirect, isAuthenticated} = useAuth0();
+  const {isLoading,error} = useAuth0();
+  
+  return (
+    <>
+      
+
+      <section className='mb-3'>
+        <MDBNavbar dark bgColor='dark'>
+          <MDBContainer fluid>
+            <MDBNavbarToggler
+              type='button'
+              className='second-button'
+              data-target='#navbarToggleExternalContent'
+              aria-controls='navbarToggleExternalContent'
+              aria-expanded='false'
+              aria-label='Toggle navigation'
+              onClick={() => setShowAnimated2(!showAnimated2)}
+            >
+              <div className={`animated-icon2 ${showAnimated2 && 'open'}`}>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+              </div>
+            </MDBNavbarToggler>
+          </MDBContainer>
+        </MDBNavbar>
+
+        <MDBCollapse show={showAnimated2} className=' bg-black'>
+          <div className='bg-light flex flex-col gap-2 bg-black shadow-3 p-4'>
+          <button type="button" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
+            <Link to='/'  className='text-white'>Home</Link>
+          </button>
+          <button type="button" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
+          <Link to='/gallery'  className='text-white'>Gallery</Link>
+          </button>
+          <button type="button" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
+          <Link to='/about-us'  className='text-white'>About us</Link>
+          </button>
+          <button type="button" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
+          <Link to='/contact-us'  className='text-white'>Contact us</Link>
+          </button>
+          
+        {error && <p>Authentication Error</p>}
+        {!error && isLoading && <p>Loading....</p>}
+         {!error && !isLoading && !isAuthenticated && 
+            <button onClick={()=>loginWithRedirect()} type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    Login / Signup
+            </button>
+        }  
+        {!error && !isLoading && isAuthenticated &&
+            <button  onClick={()=>logout()} type="button" className="flex gap-2 text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
+                
+                <img src={logoutIcon} alt="" height='20px' width='20px' />
+                {user.given_name}
+            </button>
+          
+        }
+          </div>
+        </MDBCollapse>
+      </section>
+    </>
+  );
+}
