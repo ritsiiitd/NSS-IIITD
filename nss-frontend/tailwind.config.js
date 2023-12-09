3f81a4ff1b4c66ad908ce4b25097a16a322db1ee
ritsiiitd <ritick20460@iiitd.ac.in>
2023-12-09 06:53:33 +0530

    vite config

--- nss-frontend/tailwind.config.js
+++ nss-frontend/tailwind.config.js
@@ -3,7 +3,6 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     fontSize: {
@@ -41,5 +40,5 @@ export default {
       }
     },
   },
-  plugins: [..., require('flowbite/plugin')],
+  plugins: [],
 }
