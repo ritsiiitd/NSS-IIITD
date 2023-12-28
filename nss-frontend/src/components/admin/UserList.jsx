187998e43961ad6d716f1afe1ba292bd23a4683d
ritsiiitd <ritick20460@iiitd.ac.in>
2023-12-29 00:17:37 +0530

    user list visible on adminDashboard

--- nss-frontend/src/components/admin/UserList.jsx
+++ nss-frontend/src/components/admin/UserList.jsx
@@ -2,208 +2,103 @@ import React, { useEffect, useState } from 'react';
 import { MDBBadge, MDBBtn, MDBTable, MDBTableHead, MDBTableBody } from 'mdb-react-ui-kit';
 import { useAuth0 } from '@auth0/auth0-react';
 import axios from 'axios';
+import { Col, Row } from 'react-bootstrap';
 
 
 export default function UserList() {
 
-  // const { getAccessTokenSilently } = useAuth0();
-  // const [users, setUsers] = useState([]);
 
-  // const audience = 'https://dev-fpe6a1284byzpoo5.us.auth0.com/api/v2/';
-  // const scopes = 'read:users';
-  // useEffect(() => {
-  //   const fetchUsers = async () => {
-  //     try {
-  //       const token = await getAccessTokenSilently({
-  //         audience: audience,
-  //         scope: scopes,
-  //       });
-  //       const response = await axios.get('https://dev-fpe6a1284byzpoo5.us.auth0.com/api/v2/users', {
-  //         headers: {
-  //           Authorization: `Bearer ${token}`,
-  //         },
-  //       });
-  //       setUsers(response.data);
-  //       console.log(users,"all users");
-  //     } catch (error) {
-  //       console.error('Error fetching users:', error);
-  //     }
-  //   };
+  const [allUsers,setUsers] = useState([{}]);
 
-  //   fetchUsers();
-  // }, [getAccessTokenSilently]);
-
-  return (
+  useEffect(()=>{
+    const fetchUsers = async() => {
+      try {
+        const response = await fetch('http://localhost:8080/api/v1/getUsers',{
+          method:'GET',
+          headers:{
+            'Content-Type':'application/json',
+          },
+        })
+        if(response.ok){
+          const result = await response.json();
+          console.log(result.data);
+          setUsers(result.data);
+        }
+      } catch (error) {
+        
+      }
+    }
+    fetchUsers();
+  },[]);
+  return (<>
+      <Row className="justify-content-center">
+        <Col xs={12} sm={8} lg={6}>
+          <div className="section_heading text-center wow fadeInUp">
+            <h3 className="mt-3 font-bold text-[50px] font-palanquin text-white mb-2">User List <span></span></h3>
+            <p className='mb-4 text-white'></p>
+            <div className="line"></div>
+          </div>
+        </Col>
+      </Row>
     <div className='mt-[50px] mr-[50px] flex h-[80%]'>
-
     <MDBTable className='ml-5' align='middle'>
       <MDBTableHead>
         <tr>
           <th scope='col'>Name</th>
-          <th scope='col'>Title</th>
-          <th scope='col'>Status</th>
-          <th scope='col'>Position</th>
-          <th scope='col'>Actions</th>
+          <th scope='col'>ID</th>
+          <th scope='col'>Email Veified</th>
+          <th scope='col'>Roles</th>
+          <th scope='col'>Updated at</th>
         </tr>
       </MDBTableHead>
       <MDBTableBody>
-        <tr>
-          <td>
-            <div className='d-flex align-items-center'>
-              <img
-                src='https://mdbootstrap.com/img/new/avatars/8.jpg'
-                alt=''
-                style={{ width: '45px', height: '45px' }}
-                className='rounded-circle'
-                />
-              <div className='ms-3'>
-                <p className='fw-bold mb-1'>John Doe</p>
-                <p className='text-muted mb-0'>john.doe@gmail.com</p>
+        {allUsers.map((user,index)=>(
+          <tr>
+            <td>
+              <div className='d-flex align-items-center'>
+                <img
+                  src={user.picture}
+                  alt=''
+                  style={{ width: '45px', height: '45px' }}
+                  className='rounded-circle'
+                  />
+                <div className='ms-3'>
+                  <p className='fw-bold mb-1'>{user.givenName} {user.familyName}</p>
+                  <p className='text-muted mb-0'>{user.email}</p>
+                </div>
               </div>
-            </div>
-          </td>
-          <td>
-            <p className='fw-normal mb-1'>Software engineer</p>
-            <p className='text-muted mb-0'>IT department</p>
-          </td>
-          <td>
-            <MDBBadge color='success' pill>
-              Active
-            </MDBBadge>
-          </td>
-          <td>Senior</td>
-          <td>
-            <MDBBtn color='link' rounded size='sm'>
-              Edit
-            </MDBBtn>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <div className='d-flex align-items-center'>
-              <img
-                src='https://mdbootstrap.com/img/new/avatars/8.jpg'
-                alt=''
-                style={{ width: '45px', height: '45px' }}
-                className='rounded-circle'
-                />
-              <div className='ms-3'>
-                <p className='fw-bold mb-1'>John Doe</p>
-                <p className='text-muted mb-0'>john.doe@gmail.com</p>
-              </div>
-            </div>
-          </td>
-          <td>
-            <p className='fw-normal mb-1'>Software engineer</p>
-            <p className='text-muted mb-0'>IT department</p>
-          </td>
-          <td>
-            <MDBBadge color='success' pill>
-              Active
-            </MDBBadge>
-          </td>
-          <td>Senior</td>
-          <td>
-            <MDBBtn color='link' rounded size='sm'>
-              Edit
-            </MDBBtn>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <div className='d-flex align-items-center'>
-              <img
-                src='https://mdbootstrap.com/img/new/avatars/8.jpg'
-                alt=''
-                style={{ width: '45px', height: '45px' }}
-                className='rounded-circle'
-                />
-              <div className='ms-3'>
-                <p className='fw-bold mb-1'>John Doe</p>
-                <p className='text-muted mb-0'>john.doe@gmail.com</p>
-              </div>
-            </div>
-          </td>
-          <td>
-            <p className='fw-normal mb-1'>Software engineer</p>
-            <p className='text-muted mb-0'>IT department</p>
-          </td>
-          <td>
-            <MDBBadge color='success' pill>
-              Active
-            </MDBBadge>
-          </td>
-          <td>Senior</td>
-          <td>
-            <MDBBtn color='link' rounded size='sm'>
-              Edit
-            </MDBBtn>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <div className='d-flex align-items-center'>
-              <img
-                src='https://mdbootstrap.com/img/new/avatars/6.jpg'
-                alt=''
-                style={{ width: '45px', height: '45px' }}
-                className='rounded-circle'
-                />
-              <div className='ms-3'>
-                <p className='fw-bold mb-1'>Alex Ray</p>
-                <p className='text-muted mb-0'>alex.ray@gmail.com</p>
-              </div>
-            </div>
-          </td>
-          <td>
-            <p className='fw-normal mb-1'>Consultant</p>
-            <p className='text-muted mb-0'>Finance</p>
-          </td>
-          <td>
-            <MDBBadge color='primary' pill>
-              Onboarding
-            </MDBBadge>
-          </td>
-          <td>Junior</td>
-          <td>
-            <MDBBtn color='link' rounded size='sm'>
-              Edit
-            </MDBBtn>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <div className='d-flex align-items-center'>
-              <img
-                src='https://mdbootstrap.com/img/new/avatars/7.jpg'
-                alt=''
-                style={{ width: '45px', height: '45px' }}
-                className='rounded-circle'
-                />
-              <div className='ms-3'>
-                <p className='fw-bold mb-1'>Kate Hunington</p>
-                <p className='text-muted mb-0'>kate.hunington@gmail.com</p>
-              </div>
-            </div>
-          </td>
-          <td>
-            <p className='fw-normal mb-1'>Designer</p>
-            <p className='text-muted mb-0'>UI/UX</p>
-          </td>
-          <td>
-            <MDBBadge color='warning' pill>
-              Awaiting
-            </MDBBadge>
-          </td>
-          <td>Senior</td>
-          <td>
-            <MDBBtn color='link' rounded size='sm'>
-              Edit
-            </MDBBtn>
-          </td>
-        </tr>
+            </td>
+            <td>
+              <p className='fw-normal mb-1'>{user.auth0Id}</p>
+              {/* <p className='text-muted mb-0'>IT department</p> */}
+            </td>
+            {user.emailVerified && <td>
+              <MDBBadge color='success' pill>
+                Verified
+              </MDBBadge>
+            </td>}
+            {!user.emailVerified && <td>
+              <MDBBadge color='danger' pill>
+                Not Verified
+              </MDBBadge>
+            </td>}
+            {user && user.roles?.includes('Admin') &&  <td>
+              <MDBBadge color='normal' pill>
+                admin
+              </MDBBadge>
+            </td>}
+            {user && !user.roles?.includes('Admin') && <td>-</td>}
+            
+            <td>
+              
+                {user.updatedAt}
+              
+            </td>
+          </tr>
+          
+        ))}
       </MDBTableBody>
     </MDBTable>
-  </div>
+  </div></>
   );
 }
